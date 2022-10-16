3654e34c033ce7d31f55c2d9f31ce0968d158705
Henrique Venâncio <85079432+hvenanc@users.noreply.github.com>
2022-10-16 11:08:38 -0300

    Add files via upload
    Finish

---
+++ password.py
@@ -0,0 +1,17 @@
+import random
+
+print("***** Bem-Vindo ao Gerador de Senhas! *****\n")
+
+letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*.,0123456789"
+
+numero = int(input("Quantas senhas você deseja gerar? "))
+
+tamanho = int(input("Insira o tamanho da senha: "))
+
+print("Aqui estão suas senhas:\n")
+
+for x in range(numero):
+    senhas = " "
+    for y in range(tamanho):
+        senhas += random.choice(letras)
+    print(senhas)
